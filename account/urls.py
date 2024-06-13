b6dabb13e59aa34021fef2545b1fd067e532e82c
NephygTdkingz <tdkingzict@gmail.com>
2024-06-13 12:09:10 +0100

    successfully added chartjs with yahoo finance data

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,5 @@ app_name = 'account'
 urlpatterns = [
     path('login', views.login_user_view, name="login"),
     path('customer/dashboard/', views.customer_dashboard, name="customer_dashboard"),
+    path('customer/get_data/', views.aapl_data_view, name="aapl_data_view"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.http import JsonResponse
+import datetime
+
+# my imports
+from .get_aapl_data import get_aapl_data
 
 def login_user_view(request):
     if request.method == 'POST':
@@ -19,3 +24,29 @@ def login_user_view(request):
 
 def customer_dashboard(request):
     return render(request, 'account/customer/customer_dashboard.html')
+
+
+def aapl_data_view(request):
+    data = get_aapl_data()
+    
+    if data is None:
+        return JsonResponse({'error': 'Failed to fetch AAPL data'})
+    
+    try:
+        timestamps = data['chart']['result'][0]['timestamp']
+        prices = data['chart']['result'][0]['indicators']['quote'][0]['close']
+        
+        # Convert timestamps to datetime objects
+        dates = [datetime.datetime.fromtimestamp(ts) for ts in timestamps]
+        
+        # Prepare data for Chart.js
+        chart_data = {
+            'labels': [date.strftime('%Y-%m-%d') for date in dates],
+            'prices': prices,
+        }
+        
+        return JsonResponse(chart_data)
+    except (KeyError, IndexError) as e:
+        print("Error processing AAPL data:", e)
+        return JsonResponse({'error': 'Failed to process AAPL data'})
+
