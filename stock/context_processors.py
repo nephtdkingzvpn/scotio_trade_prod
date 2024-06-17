ea71a78b376989f5db6252634eb8b27c3687bf24
NephygTdkingz <tdkingzict@gmail.com>
2024-06-17 16:45:27 +0100

    added the frontend template

--- stock/context_processors.py
+++ stock/context_processors.py
@@ -2,9 +2,12 @@ from .models import BuyStock
 
 def universal_stock_list(request):
     # Fetch objects from the database
-    stock_list = BuyStock.objects.filter(user=request.user, is_active=True)[:5]
+    if request.user.is_authenticated:
+        stock_list = BuyStock.objects.filter(user=request.user, is_active=True)[:5]
 
-    return {
-        'stock_list': stock_list,
-    }
+        return {
+            'stock_list': stock_list,
+        }
+    else:
+        return {}
 
