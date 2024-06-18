489721152ea133d4984d4c123fb91e9175157941
NephygTdkingz <tdkingzict@gmail.com>
2024-06-18 12:31:32 +0100

    started refactoring the frontend page

--- frontend/views.py
+++ frontend/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, redirect
 def home(request):
     if request.user.is_authenticated:
         return redirect('account:customer_dashboard')
-    return render(request, 'frontend/base.html')
+    return render(request, 'frontend/index.html')
