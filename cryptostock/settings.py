dcbd23c3cdab8dd1c48f174d38c4b7df605458d9
NephygTdkingz <tdkingzict@gmail.com>
2024-06-21 16:23:55 +0100

    added whitenoise to serve static

--- cryptostock/settings.py
+++ cryptostock/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,10 +141,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static'),
-]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,'static'),
+# ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
